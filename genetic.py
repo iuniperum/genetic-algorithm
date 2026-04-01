535d6135d5199eba18b2cdeccf33408cbb6e2c08
iuniperum <julpietrzak147@gmail.com>
2026-04-01 11:14:57 +0200

    update

--- genetic.py
+++ genetic.py
@@ -4,8 +4,7 @@ import sys
 import random
 
 """
-GENEROWANIE ROZWIĄZAŃ
-SPRAWDZANIE WAGI
+LISTA DO STRINGU
 """
 
 weights = []
@@ -46,7 +45,7 @@ class chromosome:
 def count_f(code):
    count = 0
    for i, bit in enumerate(code):
-       if bit == "1":
+       if bit == 1:
            count +=  prices[i]
    return count
 
@@ -54,7 +53,7 @@ def count_f(code):
 def count_weight(code):
    weight = 0
    for i, bit in enumerate(code):
-       if bit == "1":
+       if bit == 1:
            weight +=  weights[i]
    return weight
 
@@ -69,15 +68,15 @@ def check_which(list, num):
 #Probability of choosing a chromosome is proportionate to its fitness value
 def roulette(list):
    wheel = 1
-   for i, ch in enumerate(list):
-       ch.range_up = wheel
+   for ch in list:
+       ch.range_down = wheel
        wheel += ch.fitness
        wheel -= 1
-       ch.range_down = wheel
+       ch.range_up = wheel
        wheel += 1
-   parent_1 = random.randint(1, wheel - 1)
+   parent_1 = random.randint(1, wheel)
    parent_1 = (check_which(population, parent_1))
-   parent_2 = random.randint(1, wheel - 1)
+   parent_2 = random.randint(1, wheel)
    parent_2 = (check_which(population, parent_2))
    return (parent_1, parent_2)
 
@@ -89,13 +88,13 @@ def crossing_over(parent_1, parent_2):
 
 
    print(parent_1[0:point] + " " + parent_2[point:])
-   chr_1 = parent_1[0:point] + parent_2[point:]
-   child_1 = chromosome(chr_1.strip())
+   chr_1 = np.concatenate((parent_1[0:point], parent_2[point:]))
+   child_1 = chromosome(chr_1)
    buffor.append(child_1)
 
 
    print(parent_2[0:point] + " " + parent_1[point:])
-   chr_2 = parent_2[0:point] + parent_1[point:]
+   chr_2 = np.concatenate((parent_2[0:point], parent_1[point:]))
    child_2 = chromosome(chr_2)
    buffor.append(child_2)
 
@@ -105,9 +104,9 @@ def mutation (chr):
    for i, bit in enumerate(chr):
        if prob[i] <= MUTATION_PROBABILITY:
            if bit == 1:
-               bit = 0
+               chr[i] = 0
            elif bit == 0:
-               bit = 1
+               chr[i] = 1
            print ("Mutation occured! ")
 
 #FIRST POPULATION - RANDOM SOLUTIONS
@@ -117,22 +116,9 @@ for i in range(x):
     code = np.random.randint(2, size=EL_NUM)
     while(count_weight(code) > MAX_WEIGHT):
         code = np.random.randint(2, size=EL_NUM)
+    #code = ''.join(str(x) for x in code)
     new = chromosome(code)
     population.append(new)
-
-#filename = sys.argv[1]
-#with open(filename) as file:
- #  for line in file:
-  #     new = chromosome(line.strip())
-   #    population.append(new)
-
-
-for i in population:
-   for j in population:
-       if len(i.code) != len(j.code):
-           print("Chromosome lengths are not equal!")
-           sys.exit()
-
 
 for i in range(MAX_GEN):
    print("GENERATION NO. " + str(i + 1))
@@ -143,8 +129,10 @@ for i in range(MAX_GEN):
    while len(buffor) != len(population):
        pair = roulette(population)
        print("Chosen parents: ")
-       print(str(population[pair[0]].code) + ", fitness = " + str(population[pair[0]].fitness))
-       print(population[pair[1]].code + ", fitness = " + str(population[pair[1]].fitness))
+       print(population[pair[0]].code)
+       print ("fitness = " + str(population[pair[0]].fitness))
+       print(population[pair[1]].code)
+       print ("fitness = " + str(population[pair[1]].fitness))
        parents = [population[pair[0]].code, population[pair[1]].code]
        crossing_over(parents[0], parents[1])
        print("\n")
